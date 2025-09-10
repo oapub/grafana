6554543a77f14fa4e4a19747c6d19162c37130dd
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2025-09-10 12:52:13 -0400

    declare historian API

---
+++ pkg/services/ngalert/schedule/historian/models/models.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"time"
+
+	"github.com/grafana/grafana/pkg/services/ngalert/models"
+)
+
+type EvalStatus string
+
+const (
+	EvalStatusSuccess      EvalStatus = "success"
+	EvalStatusNoData       EvalStatus = "no-data"
+	EvalStatusEvalError    EvalStatus = "eval-error"
+	EvalStatusProcessError EvalStatus = "process-error"
+)
+
+type RecordOpts struct {
+	Attempt         int
+	RuleKey         models.AlertRuleKey
+	GroupKey        models.AlertRuleGroupKey
+	RuleFingerprint string
+	Status          EvalStatus
+	Error           error
+	Duration        time.Duration
+	Tick            time.Time
+	EvaluationTime  time.Time
+	Version         int64
+}
